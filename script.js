11bd4270471502b3e8c741aff10592d9894aa8fb
Greg Stead <gregstead@gmail.com>
2020-10-11 02:52:00 -0500

    Tweaked bootstrap displays

--- script.js
+++ script.js
@@ -7,34 +7,42 @@ var timeStamp = moment().format('dddd MMMM Do');
 // Attach it to the #currentDay element
 $('#currentDay').text(timeStamp);
 
-var $row = $('<div>');
-$row.attr('class','row justify-content-center mx-5');
+var TIMES = [ '6am','7am','8am','9am','10am', '11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm'];
 
-for (i=0 ; i < 10; i++) {
+for (i=0 ; i < TIMES.length; i++) {
 
+    var bgColor;
+
+    // Alternate background color
+    if (i%2 == 0) {
+        bgColor = 'bg-light text-dark'
+    } else {
+        bgColor = 'bg-secondary text-light'
+    }
+
+    // Make a new row div (bootstrap)
+    var $row = $('<div>');
+    $row.attr('class','row height-75 mt-2 rounded-right ' + bgColor);
+
+    // Make 3 column divs (bootstrap)
     var $col1 =  $('<div>');
-    $col1.attr('class','col col-2 h-100 bg-dark');
-    $col1.text('' + i + 'am');
+    $col1.attr('class','col col-2' + " text-dark bg-light  border-light border-top border-bottom");
+    $col1.text(TIMES[i]);
 
     var $col2 =  $('<div>');
-    $col2.attr('class','col col-8 h-100');
+    $col2.attr('class','col col-8 border-top border-bottom');
     $col2.text('Hello')
 
     var $col3 =  $('<div>');
-    $col3.attr('class','col col-2 h-100 themed-grid-col');
-    $col3.append('<button>').attr('class','btn').text('press me');
+    $col3.attr('class','col col-2 border-top border-bottom border-right rounded-right rounded-lg');
+    var $addEventBtn = $('<button>')
+    $addEventBtn.attr('class','btn-info').text('B');
+    $col3.append($addEventBtn);
 
+    // append columns to row
     $row.append($col1, $col2, $col3);
 
-//     <div class="row">
-//     <div class="col-1">poop
-//     </div>
-//     <div class="col-10">pooooop
-//     </div>
-//     <div class="col-1">poop
-//     </div>
-// </div>
+    // append row to timeblockEl
+    $('#timeblockEl').append($row);
 
 }
-
-$('#timeblockEl').append($row);
