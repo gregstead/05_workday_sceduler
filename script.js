92cc3b0533e510018ec50b5981ea71e01b588b62
Greg Stead <gregstead@gmail.com>
2020-10-11 14:16:35 -0500

    Got past, future, and current background colors for Event col in timeblockEl

--- script.js
+++ script.js
@@ -2,41 +2,60 @@
 var timeblockEl = $('#timeblockEl');
 
 
-// Get timestamp format from moment
-var timeStamp = moment().format('dddd MMMM Do');
+// Get timest amp format from moment
+var timeStamp = moment();
 // Attach it to the #currentDay element
-$('#currentDay').text(timeStamp);
+$('#currentDay').text(timeStamp.format('dddd MMMM Do'));
 
-var TIMES = [ '6am','7am','8am','9am','10am', '11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm'];
+var TIMES = function() {
 
-for (i=0 ; i < TIMES.length; i++) {
+    return ['06','07','08','09','10','11','12','13','14','15','16','17','18','19','20'];
+};
 
-    var bgColor;
+for (i=0 ; i < TIMES().length; i++) {
 
-    // Alternate background color
-    if (i%2 == 0) {
-        bgColor = 'bg-light text-dark'
-    } else {
-        bgColor = 'bg-secondary text-light'
+    var TIMESindex = TIMES()[i];
+    var bgColor = '';
+
+    // create a time object for each hour
+    var timeIndexObject = moment(timeStamp.format('YYYY-MM-DD') + ' ' + TIMESindex);
+
+    //// Alternate background color
+
+    // if it is now, bg-danger
+    if (timeIndexObject.isSame(timeStamp, 'hour')) {
+        bgColor = 'bg-danger';
+    } 
+    // if it is before now, bg-dark
+    else if (timeIndexObject.isBefore(timeStamp)){
+        bgColor = 'bg-dark text-white-50';
     }
+    // otherwise, bg-success
+    else {
+        bgColor = 'bg-success text-white'
+    }
+
 
     // Make a new row div (bootstrap)
     var $row = $('<div>');
-    $row.attr('class','row height-75 mt-2 rounded-right ' + bgColor);
+    $row.attr('class','row height-75 mt-2');
 
-    // Make 3 column divs (bootstrap)
+    //// Make 3 column divs (bootstrap)
+    // Hour col
     var $col1 =  $('<div>');
-    $col1.attr('class','col col-2' + " text-dark bg-light  border-light border-top border-bottom");
-    $col1.text(TIMES[i]);
+    $col1.attr('class','col col-2' + " text-dark bg-light border-light border-top border-bottom");
+    $col1.text(TIMESindex);
 
+    // Event col
     var $col2 =  $('<div>');
-    $col2.attr('class','col col-8 border-top border-bottom');
+    $col2.attr('class','col col-8 ' + bgColor);
     $col2.text('Hello')
 
+    // Button col
     var $col3 =  $('<div>');
-    $col3.attr('class','col col-2 border-top border-bottom border-right rounded-right rounded-lg');
+    $col3.attr('class','col col-2');
     var $addEventBtn = $('<button>')
-    $addEventBtn.attr('class','btn-info').text('B');
+    $addEventBtn.attr('class','btn btn-info w-100 h-100').html('<i class="far fa-calendar-plus"></i>');
     $col3.append($addEventBtn);
 
     // append columns to row
